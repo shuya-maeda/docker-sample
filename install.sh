73a5ecf5ca9f8fdfa258a939570395ebe577f54c
Shuya MAEDA <19356651+shuya-maeda@users.noreply.github.com>
2020-09-06 04:23:46 +0000

    install docker and docker-compose in Ubuntu 18.04

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+VERSION_DOCKER_COMPOSE='1.26.1'
+
+sudo apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+
+sudo apt-get update
+sudo apt install -y docker-ce
+
+
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION_DOCKER_COMPOSE}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
